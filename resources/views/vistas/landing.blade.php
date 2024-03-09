23a481d457f0ff2b0c11591ce17c9c1d22298882
lino250 <lino250v@gmail.com>
2024-03-09 13:31:39 -0400

    Creacion vista landing

---
+++ resources/views/vistas/landing.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.welcome')
+@section('title', 'Alcaldia Cumaná')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <img src="{{asset('img/alcaldiaCumana.png')}}" alt="">
+        </div>
+    </div>
+</div>
+@endsection
