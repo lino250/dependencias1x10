2647399edede4377cbe29e6e54b76ade20e53452
lino250 <lino250v@gmail.com>
2024-03-09 13:30:57 -0400

    creacion de estructura menuV

---
+++ resources/views/menus/menuV.blade.php
@@ -0,0 +1,54 @@
+<header class="navbar nav-horiz sticky-top flex-md-nowrap p-0" data-bs-theme="dark">
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#"><img src="{{asset('img/amoW.png')}}" alt=""></a>
+  <ul class="navbar-nav flex-row d-md-none">
+    <li class="nav-item text-nowrap">
+      <button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </li>
+  </ul>
+</header>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="sidebar border-right col-md-3 col-lg-2 p-0 nav-vertical">
+      <div class="offcanvas-md offcanvas-end nav-vertical" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+        <div class="offcanvas-header justify-content-center">
+          <h5 class="offcanvas-title " id="sidebarMenuLabel">Alcaldia 1X10</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
+        </div>
+        <div class="d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+                Registro 1x10
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="#">
+                Reporte
+              </a>
+            </li>
+          </ul>
+          <ul class="nav flex-column mb-auto">
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="#">
+                Configuración
+              </a>
+            </li>
+          <hr class="my-3">
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="#">
+                Cerrar Sesión
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+     
+    </main>
+  </div>
+</div>
