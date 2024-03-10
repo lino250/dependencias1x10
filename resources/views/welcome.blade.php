9091980ee51e3c530271ddab24a1147f4d248611
lino250 <lino250v@gmail.com>
2024-03-10 17:53:10 -0400

    modificacion en la vista landing y welcome

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.landing')
+
+@section('title', 'Alcaldia Cumaná')
+    
+@section('content')
+    <div class="container landing-inic">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Registra tu 1x10 </h1>
+                <img class="img-alcaldia" src="{{asset('img/alcaldiaCumana.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+@endsection
