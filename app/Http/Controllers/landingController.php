605347a29fe017aa6a4f3d260104f159e3f9c095
lino250 <lino250v@gmail.com>
2024-03-10 17:50:20 -0400

    modificacion en el controlador del landing

--- app/Http/Controllers/landingController.php
+++ app/Http/Controllers/landingController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class landindController extends Controller
+class landingController extends Controller
 {
     public function __invoke()
     {
-        return view('layouts/landing');
+        return view('welcome');
     }
 }
